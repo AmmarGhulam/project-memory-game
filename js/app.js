5fe1543f32b01668cd5303b3886c0417494211cb
Ammar Ghulam <ammar.ghulam@hotmail.com>
2018-04-23 12:38:21 +0300

    modified:   css/app.css 	modified:   index.html 	modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -10,10 +10,13 @@ let counter = document.querySelector(".moves");
 var second = 0, minute = 0; hour = 0;
 var timer = document.querySelector(".timer");
 var interval;
+const deck = document.querySelector(".deck");
+var shuffledCards = shuffle(cards);
 function startTimer(){// start the timer for the game
     interval = setInterval(function(){
         timer.innerHTML = minute+"mins "+second+"secs";
-        second++;
+        if(matchedCard < 7)
+        	second++;
         if(second == 60){
             minute++;
             second=0;
@@ -26,7 +29,17 @@ function startTimer(){// start the timer for the game
 }
 	
 function restart(){// reload the page	
-	location.reload();
+	//location.reload();
+		for (let i = 0; i < cards.length; i++){
+					cards[i].classList.remove("show", "open","match");
+				};
+		second = 0, minute = 0; hour = 0; moves=0; openedCards = []; matchedCard = 0;
+		counter.innerHTML = moves;		
+		cards = shuffle(cards);
+		shuffleCards();
+		for(let i= 0; i < 3; i++){
+            stars[i].style.visibility = "visible";
+         }
 }
 window.onload = startGame();
 
@@ -37,24 +50,18 @@ var displayCard = function (){// when click toggle the cards
     this.classList.toggle("disabled");
 };
 
-
 function startGame(){
 	// add lestiner to every card that will call openYad(),displayCard() and finish()
 	for (var i = 0; i < cards.length; i++){
-		cards[i].addEventListener("click", openYad);
+		cards[i].addEventListener("click", compare);
 		cards[i].addEventListener("click", displayCard);
 		cards[i].addEventListener("click",finish);
 	};
-	// deck of all cards in game
-	const deck = document.querySelector(".deck");
-   var shuffledCards = shuffle(cards);
-   for (var i= 0; i < shuffledCards.length; i++){
-      [].forEach.call(shuffledCards, function(item){
-         deck.appendChild(item);
-      });
-   }startTimer();
+	shuffleCards();// deck of all cards in game
+	startTimer();
 }
-function openYad(){
+
+function compare(){
 // when 2 card selected it will check its type
 	openedCards.push(this);
     var len = openedCards.length;
@@ -68,13 +75,11 @@ function openYad(){
 			}
 		}
 }
+
 function displayCard(){
 	this.classList.toggle("open");
 	this.classList.toggle("show");
 }
-
-
-   
   
   function matched(){
 				openedCards[0].classList.add("match");
@@ -86,8 +91,7 @@ function displayCard(){
 				matchedCard=matchedCard+1;
 }
 
-//for when cards don't match
-function unmatched(){
+function unmatched(){//for when cards don't match
 				openedCards[0].classList.toggle("unmatch");
 				openedCards[1].classList.toggle("unmatch");
 				disable()
@@ -103,7 +107,7 @@ function unmatched(){
     },500); 
 }
 
-  function moveCounter(){    
+function moveCounter(){    
     moves++;    
     counter.innerHTML = moves;
 	if(moves > 17 && moves < 20){//2 stars
@@ -127,24 +131,32 @@ function disable(){//user can not click on any cards
 				};
 }
 
-
 function finish(){// finish or not
 	if (matchedCard == 8){
-		 finalTime = timer.innerHTML;
-        congrats();
+		finalTime = timer.innerHTML;
+		var starRating = document.querySelector(".stars").innerHTML;
+		document.querySelector(".congrats").innerHTML ="...Congratulations... You got"+starRating+" stars  with "+moves+" moves your time is "+finalTime;
+		congrats();
 	}
 }
-function congrats(){
-	for (let i = 0; i < cards.length; i++){
-					cards[i].style.visibility = "hidden";
-				};
-        var starRating = document.querySelector(".stars").innerHTML;
-        //showing move, rating, time on modal
-        document.querySelector(".deck").innerHTML 
-		="...Congratulations... You got"+starRating+" stars  with "+moves+" moves your time is "+finalTime;
-		
-}
 
+// function congrats(){
+// 	for (let i = 0; i < cards.length; i++){
+// 					cards[i].style.visibility = "hidden";
+// 				};
+//         var starRating = document.querySelector(".stars").innerHTML;
+//         //showing move, rating, time on modal
+//         document.querySelector(".deck").innerHTML 
+// 		="...Congratulations... You got"+starRating+" stars  with "+moves+" moves your time is "+finalTime;
+		
+// }
+function shuffleCards(){
+	   for (var i= 0; i < shuffledCards.length; i++){
+      [].forEach.call(shuffledCards, function(item){
+         deck.appendChild(item);
+      });
+   }
+}
 
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -161,3 +173,28 @@ function shuffle(array) {
     return array;
 }
 
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+function congrats() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
